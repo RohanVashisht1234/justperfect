5677a6eae64edca65dfff3292c7c492454d2dd29
rohanvashisht <rohanprogrammer1@gmail.com>
2024-04-04 13:49:18 +0530

    ok

--- tests/main.f95
+++ tests/main.f95
@@ -2,6 +2,7 @@ program hello
 implicit none
     character(len=14):: name
     integer() :: age
+    
     name = "Rohan Vashisht"
     age = 18
     print *, "Hello! my name is ", name, "and my age is", age
